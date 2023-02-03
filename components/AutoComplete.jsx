a3c21bd8ecabacfb9d720a9b971bc7ce5372ca75
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2023-02-03 14:10:49 +0000

    Removing Errors

--- components/AutoComplete.jsx
+++ components/AutoComplete.jsx
@@ -15,7 +15,7 @@ export default function AutoComplete({ searchString, setSearchString }) {
     <div className="flex flex-col left-[31vw] fixed top-[10vh] cursor-default rounded-lg w-[33vw] min-w-300px h-auto bg-white z-[1000]">
       {searches.length >= 1 ? (
         searches?.map((search, index) => (
-          <div className="flex items-center my-1 hover:bg-gray-100 transition px-2 dark:hover:bg-white/10 p-1">
+          <div key={index} className="flex items-center my-1 hover:bg-gray-100 transition px-2 dark:hover:bg-white/10 p-1">
             <ClockIcon className="icon w-4 h-4 p-0 mr-2 dark:text-gray-900" />
             <span
               className="w-10/12 text-sm"
